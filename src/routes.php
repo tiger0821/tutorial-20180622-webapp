9850555d5244b6831c92f90795faa0d54a62c22b
Issei Hiraoka <issei.hiraoka@gmail.com>
2018-06-21 22:11:45 +0900

    moved to GET param

--- src/routes.php
+++ src/routes.php
@@ -5,19 +5,25 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
+    // GET parameters
+    $getParams = $request->getQueryParams();
+
+    // DB Query
     $conn = $this->dbconn;
-    $params = array();
+    $sqlParams = array();
     $tsql = "SELECT TOP 20 c.FirstName as FirstName, c.MiddleName as MiddleName, c.LastName as LastName FROM SalesLT.Customer c ";
-    if (isset($args['name'])) {
+    if (isset($getParams['name'])) {
+        $name = $getParams['name'];
+        $args['name'] = $name;
         $tsql .= "WHERE c.FirstName LIKE '%' + ? + '%' OR c.MiddleName LIKE '%' + ? + '%' OR c.LastName LIKE '%' + ? + '%'";
-        $params = array($args['name'], $args['name'], $args['name']);
+        $sqlParams = array($name, $name, $name);
     }
     $getCustomers = $conn->prepare($tsql);
-    $getCustomers->execute($params);  
+    $getCustomers->execute($sqlParams);
     $customers = $getCustomers->fetchAll(PDO::FETCH_ASSOC);  
 
     $args['customers'] = $customers;
